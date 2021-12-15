defcff2adfe26d2dc45ca431e00a0f3ba9aee9b2
selahattin ceylan <selahattinceylan9622@gmail.com>
2021-12-15 15:01:53 +0300

    userinformation service added to service layer

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,11 +1,15 @@
 package service
 
-import "github.com/furkansahinfs/AutoOrder-Backend/pkg/service/user"
+import (
+	"github.com/furkansahinfs/AutoOrder-Backend/pkg/service/user"
+	userinformation "github.com/furkansahinfs/AutoOrder-Backend/pkg/service/userInformation"
+)
 
 type Config struct{}
 
 type Service interface {
 	GetConfig() *Config
 	GetUserService() *user.Service
+	GetUserInformationService() *userinformation.Service
 	Shutdown()
 }
