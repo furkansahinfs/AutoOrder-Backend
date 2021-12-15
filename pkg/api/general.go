7c4c8c9cea6f20e7a58a3cfec0a3c0d3d124a3b9
selahattin ceylan <selahattinceylan9622@gmail.com>
2021-12-15 23:26:23 +0300

    general functions created and user from token function added

---
+++ pkg/api/general.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/furkansahinfs/AutoOrder-Backend/pkg/model"
+)
+
+func (a *API) controlUser(w http.ResponseWriter, r *http.Request) (*model.User, error) {
+	token, err := a.getToken(w, r)
+	if err != nil {
+
+		return nil, err
+	}
+	payload, err := a.verifyToken(token, a.config.SigningSecret)
+	if err != nil {
+		return nil, err
+	}
+	found, err := a.service.GetUserService().CheckExistByMail(payload.Email)
+	if found {
+		var user model.User
+		user.Email = payload.Email
+		return &user, nil
+	}
+	return nil, errors.New("User Not Found")
+
+}
--- pkg/api/user_information.go
+++ pkg/api/user_information.go
@@ -1,60 +1,61 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/furkansahinfs/AutoOrder-Backend/pkg/api/response"
+	"github.com/furkansahinfs/AutoOrder-Backend/pkg/model"
 )
 
 //endpoint for store userInformation
 func (a *API) StoreUserInformation(w http.ResponseWriter, r *http.Request) {
-	token, err := a.getToken(w, r)
+	user, err := a.controlUser(w, r)
 	if err != nil {
-		response.Errorf(w, r, fmt.Errorf("error getting storeUserInformation info: %v", err), http.StatusBadRequest, err.Error())
+		response.Errorf(w, r, fmt.Errorf("error getting updateUserInformation info: %v", err), http.StatusBadRequest, err.Error())
 		return
 	}
-
-	payload, err := a.verifyToken(token, a.config.SigningSecret)
+	var payload model.UserInformation
+	err = json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		response.Errorf(w, r, fmt.Errorf("error getting storeUserInformation info: %v", err), http.StatusBadRequest, err.Error())
+		response.Errorf(w, r, fmt.Errorf("error getting login info: %v", err), http.StatusBadRequest, err.Error())
 		return
 	}
-	a.service.GetUserService().CheckExistByMail(payload.Email)
 
 	a.service.GetUserInformationService()
 }
 
 //endpoint for update userInformation
 func (a *API) UpdateUserInformation(w http.ResponseWriter, r *http.Request) {
-	token, err := a.getToken(w, r)
+	user, err := a.controlUser(w, r)
 	if err != nil {
 		response.Errorf(w, r, fmt.Errorf("error getting updateUserInformation info: %v", err), http.StatusBadRequest, err.Error())
+		return
+	}
+	var payload model.UserInformation
+	err = json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		response.Errorf(w, r, fmt.Errorf("error getting login info: %v", err), http.StatusBadRequest, err.Error())
 		return
 	}
 
-	payload, err := a.verifyToken(token, a.config.SigningSecret)
-	if err != nil {
-		response.Errorf(w, r, fmt.Errorf("error getting updateUserInformation info: %v", err), http.StatusBadRequest, err.Error())
-		return
-	}
-	a.service.GetUserService().CheckExistByMail(payload.Email)
 	a.service.GetUserInformationService()
 }
 
 //endpoint for delete userInformation
 func (a *API) DeleteUserInformation(w http.ResponseWriter, r *http.Request) {
-	token, err := a.getToken(w, r)
+	user, err := a.controlUser(w, r)
 	if err != nil {
-		response.Errorf(w, r, fmt.Errorf("error getting deleteUserInformation info: %v", err), http.StatusBadRequest, err.Error())
+		response.Errorf(w, r, fmt.Errorf("error getting updateUserInformation info: %v", err), http.StatusBadRequest, err.Error())
+		return
+	}
+	var payload model.UserInformation
+	err = json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		response.Errorf(w, r, fmt.Errorf("error getting login info: %v", err), http.StatusBadRequest, err.Error())
 		return
 	}
 
-	payload, err := a.verifyToken(token, a.config.SigningSecret)
-	if err != nil {
-		response.Errorf(w, r, fmt.Errorf("error getting deleteUserInformation info: %v", err), http.StatusBadRequest, err.Error())
-		return
-	}
-	a.service.GetUserService().CheckExistByMail(payload.Email)
 	a.service.GetUserInformationService()
 }
